9f957bb0140a568d95bf8e7d3629125dc579283f
Kure <christian.parobek@gmail.com>
2015-11-23 21:19:02 -0500

    added 5x to cov calc

--- coverage_calculator.sh
+++ coverage_calculator.sh
@@ -1,15 +1,19 @@
 ### Coverage Calculation pipeline for Acinetobacter baumannii whole-genome sequencing
 ### Started December 19, 2014
 
-for name in `cat bamnames.list | head -2`
+for name in `cat names/bamnames.list`
 do
 
-echo $name - 
-bedtools genomecov -ibam $name -max 10 | grep "genome	10"
-
+echo $name - >> coverage/genome10.txt
+bedtools genomecov -ibam $name -max 10 | grep "genome	10" >> coverage/genome10.txt
+bedtools genomecov -ibam $name -max 5 | grep "genome	5" >> coverage/genome10.txt
 #| grep "genome 10" >> coverage/genome10.txt
 
 done
 
 
-genome	10
+
+#for name in `cat names/bamnames.list`
+#do echo $name -
+#bedtools genomecov -ibam $name -max 10 | grep "genome\t5"
+#done
